b23de240060dbfcc2ee8feb88adb0e91f40f8a9c
daegyu <dakyu26@gmail.com>
2024-07-24 17:42:26 +0900

    flask 과제1

---
+++ flask/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, Response
+import test
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, This is Main Page!"
+
+@app.route('/about')
+def about():
+    return "Hello, This is Main Page!"
+
+# 동적으로 URL 파라미터 값을 받아서 처리해준다.
+@app.route('/user/<username>')
+def user_profile(username):
+    return f'UserName : {username}'
+
+@app.route('/number/<int:number>')
+def number(number):
+    return f'Number : {number}'
+
+# post 요청 날리는 법
+# (1) postman
+# (2) requests
+import requests # pip install requests
+@app.route('/test')
+def test():
+    url = 'http://127.0.0.1:5000/submit'
+    data = 'test data'
+    response = requests.post(url=url, dta=data)
+    return response
+
+@app.route('/submit', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def submit():
+    print(request.method)
+
+    if request.method == 'GET':
+        print("GET method")
+
+    if request.method == 'POST':
+        print("***POST method***", requests.data)
+
+    return Response("Successfully submitted", status=200)
+
+    if __name__ == "__main__":
+        app.run()
